d57dfed004b3ebf5c92e0d4b39feff842cafc69a
Heiner Schaefer <heiner.schaefer@gmx.de>
2018-12-22 15:35:59 +0100

    Code reworked

--- src/piGPStracker.py
+++ src/piGPStracker.py
@@ -3,51 +3,48 @@
 import subprocess
 import shutil
 import time
+import gpsd
 import RPi.GPIO as GPIO
+import threading
 
-
-# Function: error LED on/off
-def error_LED(switch_state):
-    if str(switch_state) == 'on':
-        print('Error-LED on...\n')
-        GPIO.output(12, GPIO.HIGH)
-    elif str(switch_state) == 'off':
-        print('GPXlogger-LED off\n')
-        GPIO.output(12, GPIO.LOW)
+# Function: LED on/off
+def LED_on_off(LED, switch_state):
+    if switch_state == 1:
+        print('LED ',LED,' on')
+        GPIO.output(LED, GPIO.HIGH)
+    elif switch_state == 0:
+        print('LED ',LED,' off')
+        GPIO.output(LED, GPIO.LOW)
     else:
-        print('wrong function call!')
+        print('wrong fuction call!!!')
 
 
-# Function: gpxlogger LED on/off
-def gpxlogger_LED(switch_state):
-    if str(switch_state) == 'on':
-        print('GPXlogger-LED on\n')
-        GPIO.output(7, GPIO.HIGH)
-    elif str(switch_state) == 'off':
-        print('GPXlogger-LED off\n')
-        GPIO.output(7, GPIO.LOW)
-    else:
-        print('wrong function call!')
+def LED_blink(LED):
+    t = threading.currentThread()
+    while True:
+        while getattr(t, "do_run", True):
+            GPIO.output(LED, GPIO.HIGH)
+            time.sleep(.5)
+            GPIO.output(LED, GPIO.LOW)
+            time.sleep(1)
+        # Stopping if do_run is False
+        GPIO.output(LED, GPIO.LOW)
 
 
-# Function: python script LED on/off
-def python_LED(switch_state):
-    if str(switch_state) == 'on':
-        print('python-LED on\n')
-        GPIO.output(16, GPIO.HIGH)
-    elif str(switch_state) == 'off':
-        print('python-LED off\n')
-        GPIO.output(16, GPIO.LOW)
-    else:
-        print('wrong function call!')
-
+# Funktion to check GPS link
+def check_GPSfix():
+    packet = gpsd.get_current()
+    if packet.mode > 1:
+        return True
+    return False
+    
 
 # Function to shutdown
 def raspi_shutdown():
     print('function raspi_shutdown')
-    python_LED('off')
-    error_LED('off')
-    gpxlogger_LED('off')
+    LED_on_off(pyscriptLED,0)
+    LED_on_off(errorLED,0)
+    LED_on_off(gpxloggerLED,0)
     print('sudo shutdown -h now ...\n')
     GPIO.cleanup()
     time.sleep(.5)
@@ -108,14 +105,14 @@ def ctrl_gpxlogger(GPIOport):
         if debounce_sw_raising(GPIOport) and GPIO.input(GPIOport) == 1  and not gpxlogger_running():
             print ("-> gpxlogger start ...")
             subprocess.run(['gpxlogger', '-d', '-f', 'Track-current.gpx'])
-            gpxlogger_LED('on')
+            LED_on_off(gpxloggerLED,1)
             time.sleep(.5)
 
     else:                        # gpxlogger switch off
         if debounce_sw_falling(GPIOport) and GPIO.input(GPIOport) == 0:
             print ("gpxlogger stop ...")
             killall_gpxlogger()
-            gpxlogger_LED('off')
+            LED_on_off(gpxloggerLED,0)
             shutil.copy2('Track-current.gpx', 'Track-'+time.strftime("%Y-%m-%d_%H%M%S")+'.gpx')
             time.sleep(1)
 
@@ -153,59 +150,94 @@ def ctrl_shutdown_button(GPIOport):
 
 print('GPIO init start')
 GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
 GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # switch to control gpxlogger
 GPIO.setup(25, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # button to shutdown raspi
 
 GPIO.setup(12, GPIO.OUT) # Error LED
+errorLED = 12
 GPIO.setup(16, GPIO.OUT) # py-script LED
+pyscriptLED = 16
 GPIO.setup(7, GPIO.OUT) # gpxlogger LED
+gpxloggerLED = 7
 
 GPIO.output(12, GPIO.LOW) # Error LED off
 GPIO.output(16, GPIO.LOW) # py-script LED off
 GPIO.output(7, GPIO.LOW) # gpxlogger LED off
 print('GPIO setup done.\n')
 
+# Connect to the local gpsd
+gpsd.connect()
+
+# check GPS status
+#
+# wait until GPS fix is done:
+print('wait for GPS fix ...')
+t = threading.Thread(target=LED_blink, args=(errorLED,))
+if check_GPSfix() == False:
+    t.start()
+while check_GPSfix() == False:
+    time.sleep(2)
+print('GPS fix!')
+t.do_run = False
+
 # Make sure gpxlogger is not running
 print('make sure gpxlogger is not running...')
 if gpxlogger_running():
     print('killall gpxlogger')
-    gpxlogger_LED('on')
+    LED_on_off(gpxloggerLED,1)
     killall_gpxlogger()
     time.sleep(1)
     if not gpxlogger_running():
         print('gpxlogger stopped!\n')
-        gpxlogger_LED('off')
+        LED_on_off(gpxloggerLED,0)
     else:
         print('gpxlogger running with unknown reason ...\n')
-        error_LED('on')
-        gpxlogger_LED('on')
+        LED_on_off(errorLED,1)
+        LED_on_off(gpxloggerLED,1)
 
 
 # Check sw_logger is off
 if GPIO.input(24):
     print('gpxlogger switch on, switch it off and start again ...\n')
-    error_LED('on')
+    LED_on_off(errorLED,1)
 
 
 # End init
 
 print("PythonScript started ... \n")
 # pythonScript-LED on
-python_LED('on')
+LED_on_off(pyscriptLED,1)
 
 
+GPIO.add_event_detect(24, GPIO.BOTH, bouncetime=100)
+GPIO.add_event_detect(25, GPIO.RISING, bouncetime=500)
+
+print("Press shutdown button 5 seconds for shutdown, 2 seconds to stop program ...")
+print('or start gpxlogger ... \n')
 
 while 1:
-    # this event controls the gpxlogger
-    GPIO.add_event_detect(24, GPIO.BOTH, callback=ctrl_gpxlogger, bouncetime=100)
+    try:
+        # this event controls the gpxlogger
+        if GPIO.event_detected(24):
+            ctrl_gpxlogger(24)
 
-    print("Press shutdown button 5 seconds for shutdown, 2 seconds to stop program ...")
-    print('or start gpxlogger ... \n')
-    GPIO.wait_for_edge(25, GPIO.RISING, bouncetime=500) # button to shutdown
+        if GPIO.event_detected(25): # button to shutdown
+            if ctrl_shutdown_button(25):
+                GPIO.remove_event_detect(24)
+                GPIO.remove_event_detect(25)
+                GPIO.cleanup()
+                break
 
-    if ctrl_shutdown_button(25):
+        if check_GPSfix() == False:
+            # ErrorLED blink
+            t.do_run = True
+        else:
+            # ErrorLED dark
+            t.do_run = False
+
+    except KeyboardInterrupt:
+        print("--KeyboardInterupt--")
         GPIO.cleanup()
         break
-
-    GPIO.remove_event_detect(24)
 
